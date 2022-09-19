48d2a216226789fb7ca3e527acd3020d70c6fd0d
Yueuwu <vladdenega1@gmail.com>
2022-09-19 15:06:56 +0700

    fix: use componentDidMount for fetching users instead of fetching them during render

--- src/Components/Sidebar_components/Users/Users.jsx
+++ src/Components/Sidebar_components/Users/Users.jsx
@@ -3,10 +3,7 @@ import style from './Users.module.css'
 import * as axios from "axios";
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
