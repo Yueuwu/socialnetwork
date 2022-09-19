2fa293b8220ebfb7f8e4fe6e2b90f0a20a0fe428
Yueuwu <vladdenega1@gmail.com>
2022-09-19 11:28:58 +0700

    fix: changed func component to class component

--- src/Components/Sidebar_components/Users/Users.jsx
+++ src/Components/Sidebar_components/Users/Users.jsx
@@ -2,53 +2,59 @@ import React from 'react';
 import style from './Users.module.css'
 import * as axios from "axios";
 
-const Users = (p) => {
-    if (p.users.length <= 4) {
+class Users extends React.Component {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    constructor(props) {
+        super(props);
+
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
-                p.setUsers(
+                this.props.setUsers(
                     response.data.items
                 )
             })
-
-
     }
-    return (
-        <div className={style.wrap}>
-            <div className={style.usrs}><h1>Users</h1></div>
+
+    render() {
+        return (
+            <div className={style.wrap}>
+                <div className={style.usrs}><h1>Users</h1></div>
                 <div className={style.usrsWrap}>
-                {p.users.map(u =>
-                    <div className={style.usWrap}>
-                        <div key={u.id} className={style.usr}>
+                    {this.props.users.map(u =>
+                        <div className={style.usWrap}>
+                            <div key={u.id} className={style.usr}>
 
-                            <div className={style.avaWrap}><img className={style.ava}
-                                      src={ u.photos.small ? u.photos.small : 'https://cdn-icons-png.flaticon.com/512/2489/2489278.png'}
-                                      alt=''
+                                <div className={style.avaWrap}><img className={style.ava}
+                                                                    src={u.photos.small ? u.photos.small : 'https://cdn-icons-png.flaticon.com/512/2489/2489278.png'}
+                                                                    alt=''
                                 />
-                            </div>
-
-                            <div className={style.info}>
-                                <div className={style.infoWrap}>
-                                    <div className={style.name}>{u.name}</div>
-                                    <div className={style.about}>About me: {u.status}</div>
-                                    <div className={style.loca}>Location: {/*{u.location.city}, {u.location.country}*/}</div>
                                 </div>
-                            </div>
 
-                            <div className={style.btnWrap}>
-                                {u.followed
-                                    ? <button className={style.btnUn} onClick={() => p.unfollow(u.id)}>Unfollow</button>
-                                    : <button className={style.btnF} onClick={() => p.follow(u.id)}>Follow</button>}
+                                <div className={style.info}>
+                                    <div className={style.infoWrap}>
+                                        <div className={style.name}>{u.name}</div>
+                                        <div className={style.about}>About me: {u.status}</div>
+                                        {/*<div className={style.loca}>Location: {u.location.city}, {u.location.country}</div>*/}
+                                    </div>
+                                </div>
+
+                                <div className={style.btnWrap}>
+                                    {u.followed
+                                        ? <button className={style.btnUn}
+                                                  onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                                        : <button className={style.btnF} onClick={() => this.props.follow(u.id)}>Follow</button>}
+                                </div>
+
                             </div>
 
                         </div>
-
-                    </div>
                     )}
                 </div>
-        </div>
-    );
-};
+            </div>
+        );
+
+    }
+}
 
 export default Users;
